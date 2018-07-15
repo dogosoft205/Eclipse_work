e2175a2c39d435bedeb0e1143d21b71cae567dee
dogosoft205 <dokorean@gmail.com>
2018-07-15 20:00:55 +0900

    local 에서 변경.

--- TestForGit/src/com/dogo/FivethMain.java
+++ TestForGit/src/com/dogo/FivethMain.java
@@ -1,12 +1,10 @@
 package com.dogo;
 
-public class SecondMain {
+public class FivethMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// 2 
-		// 3
-		// 4 in local.
+
 	}
 
 }
