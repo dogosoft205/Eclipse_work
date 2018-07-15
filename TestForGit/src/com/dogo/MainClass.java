fa25714f344a627a48a97996d50fd4459c61252c
dogosoft205 <dokorean@gmail.com>
2018-07-15 14:00:32 +0900

    edit in second

--- TestForGit/src/com/dogo/MainClass.java
+++ TestForGit/src/com/dogo/MainClass.java
@@ -6,6 +6,7 @@ public class MainClass {
 		// TODO Auto-generated method stub
 		System.out.println("¾È³ç!");
 		System.out.println("¾È³ç! I am in secondBranch.");
+		System.out.println("do you lunch? I am in secondBranch too.");
 	}
 
 }
