9d2b64b539f6088b1bdbbf533f25b9a11d54c947
dogosoft205 <dokorean@gmail.com>
2018-07-15 13:58:23 +0900

    edit in master.

--- TestForGit/src/com/dogo/MainClass.java
+++ TestForGit/src/com/dogo/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("¾È³ç!");
+		System.out.println("¾È³ç! in masterBranch.");
 		System.out.println("¾È³ç! I am in secondBranch.");
 	}
 
