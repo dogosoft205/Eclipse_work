4a8092fd8c13ffd561894969f33d4b8ddb2d620c
dogosoft205 <dokorean@gmail.com>
2018-07-15 18:02:51 +0900

    ignore 추가

--- TestForGit/src/com/dogo/MainClass.java
+++ TestForGit/src/com/dogo/MainClass.java
@@ -9,6 +9,8 @@ public class MainClass {
 		System.out.println("do you lunch? I am in secondBranch too.");
 		System.out.println("I do mergied this 4 line in master.");
 		System.out.println("I changed it in master.");
+		
+		System.out.println("I changed it in master.");
 	}
 
 }
